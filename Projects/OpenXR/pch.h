8783889c05e90e42fbd88020b5cf0cfe0efddfb4
ZhangMenghe <zmh.leaves@gmail.com>
2020-10-25 16:04:13 -0700

    add oxr proj

---
+++ Projects/OpenXR/pch.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <wrl.h>
+#include <wrl/client.h>
+#include <dxgi1_4.h>
+#include <d3d11_3.h>
+#include <d2d1_3.h>
+#include <d2d1effects_2.h>
+#include <dwrite_3.h>
+#include <wincodec.h>
+#include <DirectXColors.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <agile.h>
+#include <concrt.h>
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#define XR_USE_PLATFORM_WIN32
+#define XR_USE_GRAPHICS_API_D3D11
